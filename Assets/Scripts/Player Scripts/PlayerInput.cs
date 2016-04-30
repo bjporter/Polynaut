3cf09205acec64b902890bec7b47414e76bc42cb
Brian Porter <briansuperdooper@gmail.com>
2016-04-29 21:20:29 -0400

    Fixes to FX, added stuff
    Fixed Particle FX,
    Added Placeholder models,
    Fixed Camera FX, and added falling intro

--- Assets/Scripts/Player Scripts/PlayerInput.cs
+++ Assets/Scripts/Player Scripts/PlayerInput.cs
@@ -9,14 +9,23 @@ public class PlayerInput : MonoBehaviour {
 
     private GameObject player;
     private FirstPersonController fpsController;
+    private CharacterController characterController;
     private AnalogGlitch analogGlitch;
     private DigitalGlitch digitalGlitch;
     private TiltShift tiltShift;
+    private Ramp ramp;
     private NoiseAndGrain noiseAndGrain;
     private BloomAndFlares bloomAndFlares;
     private bool gameStarted = false;
+    private bool firstTimeHitGround = false;
     private float FXAfterStartSeconds = 5f;
     private const float ONE_FRAME_60FPS = 0.0167f;
+
+
+    [SerializeField]
+    private GameObject puffOfSmoke;
+
+    private Detonator puffOfSmokeDetonator;
 
     [SerializeField]
     private Text title_start_text_layer1;
@@ -29,18 +38,21 @@ public class PlayerInput : MonoBehaviour {
 
     void Awake () {
         fpsController = gameObject.GetComponentInParent<FirstPersonController>();
+        characterController = gameObject.GetComponentInParent<CharacterController>();
         analogGlitch = gameObject.GetComponent<AnalogGlitch>();
         digitalGlitch = gameObject.GetComponent<DigitalGlitch>();
         tiltShift = gameObject.GetComponent<TiltShift>();
         noiseAndGrain = gameObject.GetComponent<NoiseAndGrain>();
         bloomAndFlares = gameObject.GetComponent<BloomAndFlares>();
+        ramp = gameObject.GetComponents<Ramp>()[1];
+        puffOfSmokeDetonator = puffOfSmoke.GetComponent<Detonator>();
 
         //Initialization
         digitalGlitch.intensity = .2f;
-        analogGlitch.scanLineJitter = .4f;
-        analogGlitch.verticalJump = .1f;
-        analogGlitch.horizontalShake = .05f;
-        analogGlitch.colorDrift = .3f;
+        analogGlitch.scanLineJitter = .414f;
+        analogGlitch.verticalJump = .02f;
+        analogGlitch.horizontalShake = .011f;
+        analogGlitch.colorDrift = .11f;
 
         //tiltShift.enabled = true;
         //bloomAndFlares.enabled = true;
@@ -63,10 +75,21 @@ public class PlayerInput : MonoBehaviour {
             if (tiltShift.isActiveAndEnabled) {
                 tiltShift.enabled = false;
                 bloomAndFlares.enabled = false;
+                ramp.enabled = false;
+                analogGlitch.enabled = false;
+                digitalGlitch.enabled = false;
             }
         }
 
         if (gameStarted) {
+            if (characterController.isGrounded) {
+                if(!firstTimeHitGround) {
+                    puffOfSmokeDetonator.Explode();
+
+                    firstTimeHitGround = true;
+                }
+            }
+
             if (Time.deltaTime >= ONE_FRAME_60FPS) { // or 1/60th a frame
                 float intensityIncrement = (Time.deltaTime / FXAfterStartSeconds); // over 3 seconds get %, and multiply by max intensity of 95
                 digitalGlitch.intensity = digitalGlitch.intensity - intensityIncrement < 0 ? 0 : digitalGlitch.intensity - intensityIncrement;
@@ -77,6 +100,7 @@ public class PlayerInput : MonoBehaviour {
                 noiseAndGrain.intensityMultiplier = (analogGlitch.colorDrift - intensityIncrement < 0 ? 0 : analogGlitch.colorDrift - intensityIncrement)/7f;
                 tiltShift.blurArea = (analogGlitch.colorDrift - intensityIncrement < 0 ? 0 : analogGlitch.colorDrift - intensityIncrement)/15;
                 bloomAndFlares.bloomIntensity = analogGlitch.horizontalShake - intensityIncrement < 0 ? 0 : analogGlitch.horizontalShake - intensityIncrement;
+                ramp.opacity = analogGlitch.horizontalShake - intensityIncrement < 0 ? 0 : analogGlitch.horizontalShake - intensityIncrement;
 
                 Color c = title_start_text_layer1.color;
                 c.a = analogGlitch.colorDrift - intensityIncrement < 0 ? 0 : analogGlitch.colorDrift - intensityIncrement;
