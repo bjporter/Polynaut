74aa51a6b6ae916a94d486820bdb9c0baf61436e
Brian Porter <briansuperdooper@gmail.com>
2016-04-30 13:38:33 -0400

    Pick up teleport gun!
    Yeeehaw

---
+++ Assets/Scripts/Teleport Gun Scripts/TeleportCollision.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportCollision : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject fpsTeleportGun;
+
+    void OnCollisionEnter(Collision target) {
+        //Debug.Log("A collision happened!");
+        if (target.gameObject.tag == "Player") {
+            //Debug.Log("We picked up the teleport gun!");
+            gameObject.SetActive(false);
+            fpsTeleportGun.SetActive(true);
+        }
+    }
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
